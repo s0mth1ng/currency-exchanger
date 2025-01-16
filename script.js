4b81e47afe8ea849f492174f7701132e1d1e5b0e
Maxim Ivanov <ma.i.ivanov@tbank.ru>
2025-01-16 13:19:50 +0300

    added chains of conversions

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ const INITIAL_RATES = [
         from: "RUB",
         to: "EUR",
         rate: 0.0095,
-        fee: 0.02,
+        fee: 0.00,
     },
     {
         from: "EUR",
@@ -35,10 +35,35 @@ const INITIAL_RATES = [
         rate: 1.03,
         fee: 0.0,
     },
+    {
+        from: "RUB",
+        to: "CYN",
+        rate: 0.0718,
+        fee: 0.3,
+    },
+    {
+        from: "CYN",
+        to: "EUR",
+        rate: 0.1312,
+        fee: 0.0,
+    },
+    {
+        from: "EUR",
+        to: "CYN",
+        rate: 7.62,
+        fee: 0.0,
+    },
+    {
+        from: "CYN",
+        to: "USD",
+        rate: 0.1354,
+        fee: 0.0,
+    },
 ]
 var CURS2RATE = Object.assign({}, ...INITIAL_RATES.map((v) => {
     return { [`${v['from']}-${v['to']}`]: { rate: v['rate'], fee: v['fee'] } };
 }));
+const CURRENCIES = [... new Set(INITIAL_RATES.flatMap(v => [v.from, v.to]))];
 
 function insertRate(rate) {
     var rates = document.getElementById("rates");
@@ -81,24 +106,84 @@ function feeChange(node) {
     CURS2RATE[curs].fee = node.value / 100;
 }
 
+// https://stackoverflow.com/a/20871714
+const permutator = (inputArr) => {
+    let result = [];
+
+    const permute = (arr, m = []) => {
+        if (arr.length === 0) {
+            result.push(m)
+        } else {
+            for (let i = 0; i < arr.length; i++) {
+                let curr = arr.slice();
+                let next = curr.splice(i, 1);
+                permute(curr.slice(), m.concat(next))
+            }
+        }
+    }
+    permute(inputArr)
+    return result;
+}
+
+function getSubsets(arr) {
+    subsets = [];
+    for (let mask = 0; mask < 1 << arr.length; mask++) {
+        var v = mask;
+        var res = [];
+        for (let i = 0; i < arr.length; i++) {
+            if (v & 1 === 1) {
+                res.push(arr[i]);
+            }
+            v >>= 1;
+        }
+        permutator(res).forEach(s => subsets.push(s));
+    }
+    return subsets;
+}
+
+function prepareChains(from, to, currencies, limit) {
+    const other = currencies.filter(v => ![from, to].includes(v));
+    const subsets = getSubsets(other).sort(v => v.length);
+    const chains = subsets.map(s => [from, ...s, to]);
+    return chains.slice(0, limit);
+}
+
 function calculate() {
     const from = document.getElementById("from").value;
     const to = document.getElementById("to").value;
     const amount = parseFloat(document.getElementById("amount").value);
 
-    const chain = [from, to];
-    const res = calculateChainResult(CURS2RATE, chain, amount)
+    output = document.getElementById("output");
+    output.innerHTML = "";
+    const chains = prepareChains(from, to, CURRENCIES, 10);
+    const chain2result = chains.map(chain => [chain, calculateChainResult(CURS2RATE, chain, amount)])
+    chain2result.sort((a, b) => b[1] - a[1]);
+    console.log(chain2result);
+    chain2result.forEach(v => {
+        const chain = v[0];
+        const res = v[1];
+        var chainNode = document.createElement("div");
+        chainNode.className = "chain";
+        var chainItems = document.createElement("div");
+        chainItems.className = "chain-items";
+        chain.forEach(cur => {
+            var curSpan = document.createElement("span");
+            curSpan.innerHTML = `<div>${cur}</div> `;
+            curSpan.className = "chain-item";
+            chainItems.appendChild(curSpan);
+        });
 
-    const resId = "res";
-    var node = document.getElementById(resId);
-    if (node === null) {
         var node = document.createElement("div");
-        node.setAttribute("id", resId);
-        document.getElementById("output").appendChild(node);
-    }
-    if (res !== null) {
-        node.innerHTML = `<span>${res}</span> <span>${to}</span>`;
-    } else {
-        node.innerHTML = `Not enough data (probably unknown currencies ${from} / ${to})`;
-    }
+        node.className = "chain-result";
+        if (res !== null) {
+            node.innerHTML = `${res} ${to}`;
+            node.style = "color:green";
+        } else {
+            node.innerHTML = "Not enough data (probably unknown currencies";
+            node.style = "color:red";
+        }
+        chainNode.appendChild(chainItems);
+        chainNode.appendChild(node);
+        output.appendChild(chainNode);
+    })
 }
